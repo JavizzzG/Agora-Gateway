e7664b299bfcab5f98108a9ab249229aaffcbca2
JavizzzG <javierstivengalvis2008@gmail.com>
2026-04-05 21:10:14 -0500

    First commit: Basic structure

---
+++ src/test/java/com/agora/Agora_Gateway/AgoraGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora.Agora_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
